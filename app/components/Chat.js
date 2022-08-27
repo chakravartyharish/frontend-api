6b997b00dd53b25dd3bafcfbba8c91378e7b1d55
Harish Kumar Chakravarty <103282023+chakravartyharish@users.noreply.github.com>
2022-08-27 18:13:14 +0200

    Update Chat.js

--- app/components/Chat.js
+++ app/components/Chat.js
@@ -24,7 +24,7 @@ function Chat() {
   }, [appState.isChatOpen])
 
   useEffect(() => {
-    socket.current = io(process.env.BACKENDURL || "your heroku dot com goes here")
+    socket.current = io(process.env.BACKENDURL || "https://reactrememberwriting.herokuapp.com")
 
     socket.current.on("chatFromServer", (message) => {
       setState((draft) => {
