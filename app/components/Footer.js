1aefca5fc440eba9b900958a19fab80b785ee64d
Harish Kumar Chakravarty <103282023+chakravartyharish@users.noreply.github.com>
2022-08-27 18:32:00 +0200

    Update Footer.js

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -20,7 +20,7 @@ function Footer() {
       <p className="m-0">
         Copyright &copy; 2020{" "}
         <a href="/" className="text-muted">
-          ComplexApp
+          My Social Media App
         </a>
         . All rights reserved.
       </p>
