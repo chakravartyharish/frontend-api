42fc6d796e8b4ce191d23a1be39ab2912b351e0e
Harish Kumar Chakravarty <103282023+chakravartyharish@users.noreply.github.com>
2023-08-22 11:10:30 +0200

    Update About.js

--- app/components/About.js
+++ app/components/About.js
@@ -6,15 +6,23 @@ function About() {
     <Page title="About Us">
       <h2>About Us!!!!!!</h2>
       <p className="lead text-muted">Full Stack Web Developer
-Paris (75) (75) 75005
+Paris, France, 75005
 hkchakravarty@gmail.com
 +33 7 69 70 98 44
-Doctorate, 14 years of experience in IT and its applications (Chemistry), 4 years of experience in Software / Web Development, Bioinformatics, Chemoinformatics, international profile (India, Germany, USA, France).
-
-Expertise in Software Development, Web Development, Modeling and Computing Techniques, and Technologies, Chemistry and its Applications (Combustion).
-
-Here is a link to a web application I built recently (Technology used: React, Javascript, Node, MongoDB, Express, HTML, CSS):
-
+I am a senior software Developer with more than five years of
+experience in full-stack web development. I use cutting-edge
+technologies such as Python, React, Node.js, REST API, MySQL,
+MongoDB, Java, Java EE, Spring Boot, GraphQL, and Material UI to
+create Full Stack Web Applications that support the scientific and
+engineering projects of ONERA and its partners.
+In addition to my web development skills, I have a strong
+background in computational fluid dynamics, combustion modeling,
+and chemical kinetics, which I apply to implement and optimize
+numerical methods and algorithms in Python. I have also published
+several papers in peer-reviewed journals and presented my
+research at international conferences. I am passionate about
+solving complex problems and creating innovative solutions that
+advance the field of aerospace research.
 https://harishsocialmediaapp.netlify.app</p>
     </Page>
   )
